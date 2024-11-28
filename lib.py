33f6fa7e623402d270720316b94b236f1a881e9b
SrideviGouler <sridevig.8990@gmail.com>
2024-11-28 11:03:33 +0530

    Add files via upload

---
+++ lib.py
@@ -0,0 +1,9 @@
+class SeleniumWrapper:
+    def __init__(self,driver):
+        self.driver=driver
+
+    def click_on_element(self,logical_name):
+        self.driver.find_element(*logical_name).click()
+
+    def enter_data(self,logical_name,data):
+        self.driver.find_element(*logical_name).send_keys(data)
